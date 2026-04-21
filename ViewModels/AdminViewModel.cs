f2a10c64f564318e613da864a204a7ab6f467526
Sophia <zhuang.li@sao.so>
2026-04-21 17:04:18 +0800

    update

--- ViewModels/AdminViewModel.cs
+++ ViewModels/AdminViewModel.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using IndustrialControlMAUI.Services;
 using System.Text.Json.Nodes;
 
 namespace IndustrialControlMAUI.ViewModels;
