7134faa638fffe720f4b3cd60e197b0e69f12d50
Sophia <zhuang.li@sao.so>
2026-04-23 17:41:57 +0800

    update code

--- Models/ProductionDeviceMoldDto.cs
+++ Models/ProductionDeviceMoldDto.cs
@@ -5,6 +5,7 @@ public class DeviceMoldRelationDto
     public string? id { get; set; }
     public string? deviceCode { get; set; }
     public string? deviceName { get; set; }
+    public string? deviceModel { get; set; }
     public string? moldCode { get; set; }
     public string? moldModel { get; set; }
     public string? moldInstallTime { get; set; }
