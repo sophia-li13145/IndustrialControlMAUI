fbc5a4c54a13dbf151afd94638588a0fa93b612d
sophia-li13145 <1328044391@qq.com>
2026-05-20 17:47:37 +0800

    feat(material-frame): add frame query page with paged fuzzy search

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -104,6 +104,7 @@ namespace IndustrialControlMAUI
             builder.Services.AddTransient<InventorySearchViewModel>();
             builder.Services.AddTransient<FlexibleStockCheckViewModel>();
             builder.Services.AddTransient<StockCheckSearchViewModel>();
+            builder.Services.AddTransient<MaterialFrameQueryViewModel>();
 
             // ===== 注册 Pages（DI 创建）=====
             builder.Services.AddTransient<Pages.LoginPage>();
@@ -161,6 +162,7 @@ namespace IndustrialControlMAUI
             builder.Services.AddTransient<InventorySearchPage>();
             builder.Services.AddTransient<FlexibleStockCheckPage>();
             builder.Services.AddTransient<StockCheckSearchPage>();
+            builder.Services.AddTransient<MaterialFrameQueryPage>();
 
 
             builder.Services.AddTransient<WarehouseLocationPickerPage>();
