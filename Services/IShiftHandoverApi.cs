87575908834083114306840ef846e5472a2b141c
sophia-li13145 <1328044391@qq.com>
2026-08-07 12:29:38 +0800

    feat: add handover record detail sheet

---
+++ Services/IShiftHandoverApi.cs
@@ -0,0 +1,15 @@
+using IndustrialControlMAUI.Models;
+
+namespace IndustrialControlMAUI.Services;
+
+public interface IShiftHandoverApi
+{
+    Task<ApiResp<PendingShiftHandover>> GetPendingHandoverAsync(CancellationToken ct = default);
+    Task<ApiResp<ShiftHandoverPageResult>> GetRecordsAsync(int pageNo, int pageSize, CancellationToken ct = default);
+    Task<ApiResp<List<ShiftHandoverDictionary>>> GetDictListAsync(CancellationToken ct = default);
+    Task<ApiResp<ShiftHandoverInfo>> GetHandoverInfoAsync(CancellationToken ct = default);
+    Task<ApiResp<List<ReceiverTeamOption>>> GetReceiverTeamOptionsAsync(CancellationToken ct = default);
+    Task<ApiResp<bool>> AddAsync(AddShiftHandoverRequest request, CancellationToken ct = default);
+    Task<ApiResp<ShiftHandoverDetail>> GetDetailAsync(string id, CancellationToken ct = default);
+    Task<ApiResp<bool>> ConfirmHandoverAsync(ConfirmShiftHandoverRequest request, CancellationToken ct = default);
+}
